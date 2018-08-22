49bd8629c1273c7979fbe6231c416b36b7c5c8e9
eloisa froes <ola.eloisa@gmail.com>
2018-08-22 15:45:50 -0300

    fixed the damn navbar again

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -4,6 +4,7 @@ function initUpdateNavbarOnScroll() {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight - 500) {
         navbar.classList.add('navbar-wagon-white');
+        console.log('oie');
       } else {
         navbar.classList.remove('navbar-wagon-white');
       }
