f4d33a6c4abebb75aa1ca254b1f4da70c1f1dbad
eloisafroes <ola.eloisa@gmail.com>
2018-08-24 13:32:57 -0300

    fixed navbar js

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -4,7 +4,6 @@ function initUpdateNavbarOnScroll() {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight - 500) {
         navbar.classList.add('navbar-wagon-white');
-        console.log('oie');
       } else {
         navbar.classList.remove('navbar-wagon-white');
       }
