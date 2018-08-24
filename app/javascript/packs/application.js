e45ae55d5c5f74e8cfbc5fae4799be7d3983ba2b
Michael Resnick <michaeln.resnick@gmail.com>
2018-08-24 16:38:16 -0300

    tom light this joint

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,4 +4,5 @@ import { dragnDrop } from '../components/dragndrop_list';
 dragnDrop();
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
-
+import { bindSweetAlertButtonDemo } from '../components/sweetalert';
+bindSweetAlertButtonDemo();
