5205823aa78da90f25c910a54da0ce37e62dd702
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-08-27 16:18:45 -0300

    make some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ def edit
 
 end
 
-# def sort
-#   params[:category].each_with_index do |id, index|
-#     Category.where(id: id).update_all(position: index + 1)
-#   end
-#   head :ok
-# end
+def sort
+  params[:category].each_with_index do |id, index|
+    Category.where(id: id).update_all(position: index + 1)
+  end
+  head :ok
+end
 
 def update
   sort
