557f301faee8c18807415ebaa3cb8873316a0f4a
eloisafroes <ola.eloisa@gmail.com>
2018-08-29 11:45:27 -0300

    added custom title tags for each view of our lovely app!

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_220424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "position"
-    t.boolean "deny", default: false
+    t.boolean "deny", default: true
   end
 
   create_table "margin_calls", force: :cascade do |t|
