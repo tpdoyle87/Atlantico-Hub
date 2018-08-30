e2ee746eac65b76820380544cb8d2afd65b2bf43
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-08-30 18:37:48 -0300

    fixing color

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @categories = Category.denied
+    @margin_color = "#EE5F5B"
     # @categories = Category.where(deny: false).order(:position)
   end
-
-  def edit
-
-  end
-
 
 end
 
